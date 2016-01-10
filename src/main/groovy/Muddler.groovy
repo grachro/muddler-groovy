b4b5418480bc5ae37f32a2dc51254d3ace26504f
grachro <grachro@gmail.com>
2016-01-10 22:19:18 +0900

    Routesを動的に変更

--- src/main/groovy/Muddler.groovy
+++ src/main/groovy/Muddler.groovy
@@ -4,10 +4,14 @@ import static spark.Spark.get
  * Created by grachro on 2016/01/10.
  */
 class Muddler {
-    public static void main(String[] args) {
-        get "/hello", {req, res ->
 
-            def f = new File("script/SomethingString.groovy")
+    static String SCRIPT_ROOT = "script"
+
+    public static void main(String[] args) {
+        get "/:path1", {req, res ->
+
+            def path1 = req.params(":path1")
+            def f = new File("${Muddler.SCRIPT_ROOT}/${path1}.groovy")
             def groovyString = f.getText()
 
             def binding = [
